3a01269b797da0cd4635161c3abb4cba9457bc05
longdvgch <43132348+longdvgch@users.noreply.github.com>
2023-07-11 11:06:08 +0700

    Update functional_ui_test.py

--- automatedtesting/selenium/functional_ui_test.py
+++ automatedtesting/selenium/functional_ui_test.py
@@ -10,9 +10,6 @@ import logging
 # Start the browser and login with standard_user
 
 def functional_ui_test(user, password):
-
-    logging.basicConfig(filename="./selenium.txt", format="%(asctime)s %(message)s", filemode="w", level=logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
-
     logging.info('Starting the browser...')
 
     # --uncomment when running in Azure DevOps.
